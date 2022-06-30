0371d13edc63add4f5765c2a83ae72cb210cf845
Bandar <bandar5mubarky@gmail.com>
2022-07-01 02:43:05 +0300

    edit for like button

---
+++ public/javascripts/contentActions.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    $("button").click(function(event){
+ 
+        event.preventDefault();
+        
+        let id = $("#id").val();
+
+        $.ajax({
+            type: "GET",
+            url:"../SubjectLike/"+id,
+            data:id,
+			success : function(user) {
+                document.getElementById('like').style.display = 'none';
+               // $("#liked").html('<i class="fa-solid fa-heart"></i>');
+                document.getElementById('liked').style.display = 'block';
+
+
+
+			},
+			error : function(e) {
+				alert("Error!")
+				console.log("ERROR: ", e);
+			}
+        });
+    });
+
+
+});
