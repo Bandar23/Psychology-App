bd85d6f3b20439db204c57d9a2765416692eb383
Bandar <bandar5mubarky@gmail.com>
2022-07-21 00:50:29 +0300

    edit text for si and sb pages

--- public/javascripts/sending.js
+++ public/javascripts/sending.js
@@ -1,10 +1,4 @@
 
-function ch(){
-
-document.querySelector('.all-content').style.display = 'none';
-
-
-
-
-
-}
+let images = ['pexels-anna-shvets-4226119.jpg','pexels-olenka-sergienko-3646172.jpg','pexels-stanislav-kondratiev-2908984.jpg'];
+let img = Math.floor(Math.random() * images.length);
+document.getElementById('img').src = "/images/"+images[img];
