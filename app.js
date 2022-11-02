ecab7226913a95b1f4e06e36e67f97a4d5743de5
Bandar <bandar5mubarky@gmail.com>
2022-11-02 09:08:58 +0300

    stanaders css

--- app.js
+++ app.js
@@ -46,8 +46,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: "10mb", extended: true}));
+app.use(express.urlencoded({limit: "10mb", extended: true, parameterLimit: 50000}));
 app.use(cookieParser());
 
 app.use(expressSession({
